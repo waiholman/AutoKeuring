58916fbf43709af587f2a823f7e52b1bd45010c3
Crater <craterboy@gmail.com>
2015-01-05 10:58:52 +0100

    Added all text

--- app/src/main/java/galip/autokeuring/ScreenFragment.java
+++ app/src/main/java/galip/autokeuring/ScreenFragment.java
@@ -26,7 +26,7 @@ public class ScreenFragment extends Fragment {
         TextView NavigationView = (TextView) getView().findViewById(R.id.txtStep);
         NavigationView.setText(Header);
     }
-//marek
+//marekd
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
